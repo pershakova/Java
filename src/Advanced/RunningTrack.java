0d24945295b520b4382d0f51e476407231dd2a40
olga <pershakova@bk.ru>
2020-03-24 22:40:52 +0300

    Task_1

---
+++ src/Advanced/RunningTrack.java
@@ -0,0 +1,13 @@
+package Advanced;
+
+public class RunningTrack extends Barrier {
+    private final int length;
+
+    public RunningTrack(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}

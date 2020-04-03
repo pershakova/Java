fe9b7a91ca8a2ab388f9d784421fbacce57305dc
olga <pershakova@bk.ru>
2020-04-03 23:45:23 +0300

    Task_3

---
+++ src/Collections/Duplicate.java
@@ -0,0 +1,48 @@
+package Collections;
+
+import java.util.*;
+
+public class Duplicate {
+    public static void main(String[] args){
+
+        printUniqueItems();
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add("bat","300");
+        phoneBook.add("cat","301");
+        phoneBook.add("goat","302");
+        phoneBook.get("goat");
+        phoneBook.get("bat");
+        phoneBook.get("owl");
+    }
+
+    private static void printUniqueItems(){
+        List<String> list = getInitialList();
+
+        Set<String> set = new HashSet<>(list);
+
+        for (String uniqueItem : set) {
+            System.out.printf("%s: %d%n", uniqueItem, Collections.frequency(list, uniqueItem));
+        }
+    }
+
+    private static List<String> getInitialList(){
+        List<String> list = new ArrayList<>();
+
+        list.add("cat");
+        list.add("dog");
+        list.add("rat");
+        list.add("bat");
+        list.add("cat");
+        list.add("cat");
+        list.add("rat");
+        list.add("dad");
+        list.add("hen");
+        list.add("pig");
+        list.add("dog");
+        list.add("cat");
+
+        return  list;
+    }
+}
---
+++ src/Collections/PhoneBook.java
@@ -0,0 +1,77 @@
+package Collections;
+
+import java.util.*;
+
+public class PhoneBook {
+
+    private Map<String, List<String>> listMap;
+
+    public PhoneBook(){
+        this.listMap = getInitialList();
+    }
+
+    private  Map<String, List<String>> getInitialList(){
+        Map<String, List<String>> map = new HashMap<>();
+
+        map.put("cat", feelArray("123", "456", "911"));
+        map.put("dog", feelArray("345"));
+        map.put("rat", feelArray("678", "ac5"));
+        map.put("bat", feelArray("300", "790"));
+        map.put("pig", feelArray("123", "765", "987"));
+        map.put("hen", feelArray("908"));
+
+        return  map;
+    }
+
+    public void add(String name, String number){
+        if (name.equals("")){
+            System.out.println("Name is empty");
+            return;
+        }
+        if (number.equals("")){
+            System.out.println("Number is empty");
+            return;
+        }
+
+        if (!listMap.containsKey(name)){
+            listMap.put(name, feelArray(number));
+            System.out.printf("Name %s with phone %s was added%n", name, number);
+        }
+        else{
+            List<String> phones = listMap.get(name);
+            if (!phones.contains(number)){
+                phones.add(number);
+                System.out.printf("Name %s with phone %s was added%n", name, number);
+            }
+            else{
+                System.out.printf("Name %s with phone %s already exists%n", name, number);
+            }
+        }
+    }
+
+    public void get(String name){
+        if (name.equals("")){
+            System.out.println("Name is empty");
+            return;
+        }
+
+        if (!listMap.containsKey(name)){
+            System.out.printf("Name %s does not exist in phone book %n", name);
+            return;
+        }
+
+        System.out.printf("Numbers for name %s:%n", name);
+
+        for (String phone : listMap.get(name)){
+            System.out.println(phone);
+        }
+    }
+
+    private ArrayList<String> feelArray(String...items){
+        ArrayList<String> temp = new ArrayList<>();
+
+        Collections.addAll(temp, items);
+
+        return temp;
+    }
+}
